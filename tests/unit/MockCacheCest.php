053b8f62bb233e76d12fa6b294d3005d0bb762eb
Hans Erik Jepsen <haeje@jfmedier.dk>
2022-08-15 13:30:56 +0200

    Updated PHP requirements to >=8.0 and updated tests to codeception 5 #CORE-746

--- tests/unit/MockCacheCest.php
+++ tests/unit/MockCacheCest.php
@@ -1,26 +1,24 @@
 <?php
 
+namespace Kodus\Cache\Test\Unit;
+
+use DateInterval;
 use Kodus\Cache\MockCache;
 use Kodus\Cache\InvalidArgumentException;
+use UnitTester;
 
 class MockCacheCest
 {
-    /**
-     * @var int
-     */
     const DEFAULT_EXPIRATION = 86400;
 
-    /**
-     * @var MockCache
-     */
-    protected $cache;
+    protected MockCache $cache;
 
-    public function _before()
+    public function _before(): void
     {
         $this->cache = new MockCache(self::DEFAULT_EXPIRATION);
     }
 
-    public function setGetAndDelete(UnitTester $I)
+    public function setGetAndDelete(UnitTester $I): void
     {
         $I->assertTrue($this->cache->set("key1", "value1"));
         $I->assertTrue($this->cache->set("key2", "value2"));
@@ -34,26 +32,26 @@ class MockCacheCest
         $I->assertSame(null, $this->cache->get("key1"));
         $I->assertSame("value2", $this->cache->get("key2"));
 
-        $I->expectException(InvalidArgumentException::class, function () {
+        $I->expectThrowable(InvalidArgumentException::class, function () {
             $this->cache->set("key@", "value1");
         });
 
-        $I->expectException(InvalidArgumentException::class, function () {
+        $I->expectThrowable(InvalidArgumentException::class, function () {
             $this->cache->get("key@");
         });
 
-        $I->expectException(InvalidArgumentException::class, function () {
+        $I->expectThrowable(InvalidArgumentException::class, function () {
             $this->cache->delete("key@");
         });
     }
 
-    public function getNonExisting(UnitTester $I)
+    public function getNonExisting(UnitTester $I): void
     {
         $I->assertSame(null, $this->cache->get("key"));
         $I->assertSame("default", $this->cache->get("key", "default"));
     }
 
-    public function expirationInSeconds(UnitTester $I)
+    public function expirationInSeconds(UnitTester $I): void
     {
         $this->cache->set("key", "value", 10);
 
@@ -67,7 +65,7 @@ class MockCacheCest
         $I->assertSame("default", $this->cache->get("key", "default"));
     }
 
-    public function expirationByInterval(UnitTester $I)
+    public function expirationByInterval(UnitTester $I): void
     {
         $interval = new DateInterval("PT10S");
 
@@ -83,7 +81,7 @@ class MockCacheCest
         $I->assertSame("default", $this->cache->get("key", "default"));
     }
 
-    public function expirationByDefault(UnitTester $I)
+    public function expirationByDefault(UnitTester $I): void
     {
         $this->cache->set("key", "value");
 
@@ -97,7 +95,7 @@ class MockCacheCest
         $I->assertSame("default", $this->cache->get("key", "default"));
     }
 
-    public function expirationInThePast(UnitTester $I)
+    public function expirationInThePast(UnitTester $I): void
     {
         $this->cache->set("key1", "value1", 0);
         $this->cache->set("key2", "value2", -10);
@@ -106,7 +104,7 @@ class MockCacheCest
         $I->assertSame("default", $this->cache->get("key2", "default"));
     }
 
-    public function clear(UnitTester $I)
+    public function clear(UnitTester $I): void
     {
         // add some values that should be gone when we clear cache:
 
@@ -124,7 +122,7 @@ class MockCacheCest
         $I->assertSame("default", $this->cache->get("key2", "default"));
     }
 
-    public function testGetAndSetMultiple(UnitTester $I)
+    public function testGetAndSetMultiple(UnitTester $I): void
     {
         $this->cache->setMultiple(["key1" => "value1", "key2" => "value2"]);
 
@@ -132,24 +130,24 @@ class MockCacheCest
 
         $I->assertSame(["key1" => "value1", "key2" => "value2", "key3" => false], $results);
 
-        $I->expectException(InvalidArgumentException::class, function () {
+        $I->expectThrowable(InvalidArgumentException::class, function () {
             $this->cache->getMultiple("Invalid type");
         });
 
-        $I->expectException(InvalidArgumentException::class, function () {
+        $I->expectThrowable(InvalidArgumentException::class, function () {
             $this->cache->setMultiple("Invalid type");
         });
 
-        $I->expectException(InvalidArgumentException::class, function () {
+        $I->expectThrowable(InvalidArgumentException::class, function () {
             $this->cache->setMultiple(["Invalid key@" => "value1"]);
         });
 
-        $I->expectException(InvalidArgumentException::class, function () {
+        $I->expectThrowable(InvalidArgumentException::class, function () {
             $this->cache->getMultiple(["Invalid key@"]);
         });
     }
 
-    public function testDeleteMultiple(UnitTester $I)
+    public function testDeleteMultiple(UnitTester $I): void
     {
         $this->cache->setMultiple(["key1" => "value1", "key2" => "value2", "key3" => "value3"]);
 
@@ -159,16 +157,16 @@ class MockCacheCest
 
         $I->assertSame("value3", $this->cache->get("key3"));
 
-        $I->expectException(InvalidArgumentException::class, function () {
+        $I->expectThrowable(InvalidArgumentException::class, function () {
             $this->cache->deleteMultiple("Invalid type");
         });
 
-        $I->expectException(InvalidArgumentException::class, function () {
+        $I->expectThrowable(InvalidArgumentException::class, function () {
             $this->cache->deleteMultiple(["Invalid key@"]);
         });
     }
 
-    public function testHas(UnitTester $I)
+    public function testHas(UnitTester $I): void
     {
         $this->cache->set("key", "value");
 
